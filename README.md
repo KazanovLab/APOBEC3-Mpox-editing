f0926959b4641f063fc98ca34a22688dd6a16c61
AlisaLyskova <alisalyskovaaa@gmail.com>
2024-09-14 13:36:04 +0300

    Add existing file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # APOBEC3-deaminase-editing-in-mpox-virus
 DOI: 10.1126/science.adg8116
+
+Original file with APOBEC3 sites is located in supplementary materials hmpxv-apobec3-dd7a582/data/B.1/B.1_2022-08-22.og.aln.pruned.tree.amino_acid.reconstruction.csv
+
+APOBEC3_sites.ipynb contains transformation of original dataframe with APOBEC3 sites, programs for searching amino acids in these positions and potential target sites for APOBEC in MPOX genome. Received tables are saved in data/article_pos_aa_edited.csv and data/APOBEC_targets_aa.csv
+
+
+Barplot data/APOBEC_targets_plus_observed.png was received using R (R_barplot.Rmd).
+
+Than we calculated the Grantham score for hypothetical amino acid changes (grantham_score.R). The plot data/observed_mutated_aa_plot.png was received using observed_mutated_aa_plot.py
---
+++ grantham_score.R
@@ -0,0 +1,38 @@
+# Calculate grantham score
+
+grantham <- readr::read_tsv("https://gist.githubusercontent.com/danielecook/501f03650bca6a3db31ff3af2d413d2a/raw/5583a134b36b60762be6cd54002a0f4044338cd0/grantham.tsv") %>%
+  tidyr:: gather(SECOND,SCORE, -FIRST) %>% dplyr::filter(SCORE > 0)  
+
+calculate_grantham <- function(a1, a2) {
+  (grantham %>% dplyr::filter(FIRST == a1, SECOND == a2))$SCORE
+}
+
+grantham
+
+df <- read.csv("data/article_pos_aa_edited.csv", sep = "\t", header = TRUE)
+df_CDS <- df %>% filter(mutation_category != "intergenic")
+score <- mapply(calculate_grantham, df_CDS$parent_aa, df_CDS$mutated_aa, SIMPLIFY = FALSE)
+score <- as.numeric(score)
+df_CDS$grantham_score <- score
+df_CDS[is.na(df_CDS)] <- 0
+
+#0–50 conservative, yellow; 51–100 moderately conservative, pink; 101–150 moderately radical, blue; >150 radical, brown; +synonymous, green1
+df_CDS <- df_CDS %>% mutate(grantham_rank = case_when(
+  grantham_score > 50 & grantham_score < 101 ~ "moderately conservative",
+  grantham_score > 100 & grantham_score < 151 ~ "moderately radical",
+  grantham_score > 149 ~ "radical",
+  grantham_score < 51 & mutation_category != "synonymous" ~ "conservative",
+  mutation_category == "synonymous" ~ "synonymous",
+))
+df_CDS <- df_CDS %>% mutate(grantham_rank_color = case_when(
+  grantham_score > 50 & grantham_score < 101 ~ pink,
+  grantham_score > 100 & grantham_score < 151 ~ blue,
+  grantham_score > 149 ~ brown,
+  grantham_score < 51 & mutation_category != "synonymous" ~ yellow,
+  mutation_category == "synonymous" ~ green1,
+))
+
+df_CDS$parent_aa[df_CDS$mutation_category == 'synonymous'] <- '='
+df_CDS$mutated_aa[df_CDS$mutation_category == 'synonymous'] <- '='
+
+write.csv(df_CDS, "data/grantham_score_article_pos_aa.csv", row.names = FALSE)
